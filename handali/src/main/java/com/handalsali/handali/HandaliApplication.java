0ba0a1066bd1237c8671eb3b70861a884b04b9db
HaDaHyeon <hada3817@gmail.com>
2024-12-31 20:05:42 +0900

    feat: make project

---
+++ handali/src/main/java/com/handalsali/handali/HandaliApplication.java
@@ -0,0 +1,13 @@
+package com.handalsali.handali;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandaliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HandaliApplication.class, args);
+	}
+
+}
