fb9603ba040089e76f49d7a0ed25ba97702fe7d4
HaDaHyeon <hada3817@gmail.com>
2025-01-01 23:08:30 +0900

    Revert "Initial commit"
    This reverts commit 382c07d5da5365494747924c2675ca7503734842.

--- src/main/java/schedule/repository/BoardRepository.java
+++
@@ -1,7 +0,0 @@
-package schedule.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import schedule.model.Board;
-
-public interface BoardRepository extends JpaRepository<Board, Long> {
-}
