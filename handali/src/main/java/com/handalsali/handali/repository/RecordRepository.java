1aa642d900b2fd01dad5b728e0d9a681979a3264
HaDaHyeon <hada3817@gmail.com>
2025-01-12 16:17:07 +0900

    feat: update recordRepository

--- handali/src/main/java/com/handalsali/handali/repository/RecordRepository.java
+++ handali/src/main/java/com/handalsali/handali/repository/RecordRepository.java
@@ -1,6 +1,7 @@
 package com.handalsali.handali.repository;
 
 import com.handalsali.handali.domain.Habit;
+import com.handalsali.handali.domain.Record;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
