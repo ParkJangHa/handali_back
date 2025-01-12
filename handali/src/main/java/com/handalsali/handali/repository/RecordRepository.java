288afd97cb5116c91cf40edb219f34cede74cf80
HaDaHyeon <hada3817@gmail.com>
2025-01-12 16:09:07 +0900

    feat: update recordRepository

--- handali/src/main/java/com/handalsali/handali/repository/RecordRepository.java
+++ handali/src/main/java/com/handalsali/handali/repository/RecordRepository.java
@@ -1,8 +1,12 @@
 package com.handalsali.handali.repository;
 
+import com.handalsali.handali.domain.Habit;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+
 @Repository
 public interface RecordRepository extends JpaRepository<Record, Long> {
+    boolean existsByHabitAndDate(Habit habit, LocalDate date);
 }
