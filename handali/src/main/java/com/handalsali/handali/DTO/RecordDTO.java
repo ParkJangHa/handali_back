4532c43b45910f81fe4ac6f4370c428a2681b3c4
HaDaHyeon <hada3817@gmail.com>
2025-01-12 21:24:42 +0900

    feat: 습관 기록 DTO

---
+++ handali/src/main/java/com/handalsali/handali/DTO/RecordDTO.java
@@ -0,0 +1,25 @@
+package com.handalsali.handali.DTO;
+
+import com.handalsali.handali.enums_multyKey.Categoryname;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+public class RecordDTO {
+    @Data
+    public static class recordTodayHabitRequest{
+        private Categoryname category;
+        private String detailed_habit_name;
+        private float time;
+        private int satisfaction;
+        private LocalDate date;
+
+    }
+    @Data
+    @AllArgsConstructor
+    public static class recordTodayHabitResponse{
+        private long record_id;
+        private String message;
+    }
+}
