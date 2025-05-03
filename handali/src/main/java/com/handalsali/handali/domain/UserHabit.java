b94b293ac8c0646e91ce608e0a862692d5e2eb64
HaDaHyeon <hada3817@gmail.com>
2025-05-03 15:35:44 +0900

    feat: 외래키 제약조건 추가

--- handali/src/main/java/com/handalsali/handali/domain/UserHabit.java
+++ handali/src/main/java/com/handalsali/handali/domain/UserHabit.java
@@ -18,11 +18,13 @@ public class UserHabit {
     private long userHabitId;
 
     @ManyToOne
-    @JoinColumn(name="user_id",nullable = false)
+    @JoinColumn(name="user_id",
+            foreignKey = @ForeignKey(foreignKeyDefinition = "FOREIGN KEY (user_id) REFERENCES user(user_id) ON DELETE CASCADE ON UPDATE CASCADE"))
     private User user;
 
     @ManyToOne
-    @JoinColumn(name="habit_id",nullable = false)
+    @JoinColumn(name = "habit_id", nullable = false,
+            foreignKey = @ForeignKey(foreignKeyDefinition = "FOREIGN KEY (habit_id) REFERENCES habit(habit_id) ON DELETE CASCADE ON UPDATE CASCADE"))
     private Habit habit;
 
     @Column
